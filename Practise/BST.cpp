f749df39f48287f4475489cba2b129aaf978099a
pratyushgta <pratyushgta@gmail.com>
2022-03-23 12:44:50 +0530

    23-03-2022

--- Practise/BST.cpp
+++ Practise/BST.cpp
@@ -9,30 +9,32 @@ struct tree
     struct tree *right;
 };
 
-struct tree *root = NULL;
+struct tree *root = NULL;[]
 
 void create()
 {
 }
 
-void insert(struct tree *r, int val)
+struct tree *insert(struct tree *r, int val)
 {
     struct tree *newnode = new tree;
+    newnode->data = val;
     newnode->left = NULL;
     newnode->right = NULL;
     if (r == NULL)
     {
-        newnode->data = val;
-        root = newnode;
+        // newnode->data = val;
+        r = newnode;
     }
     else if (val < r->data)
     {
-        insert(r->left, val);
+        r->left = insert(r->left, val);
     }
     else
     {
-        insert(r->right, val);
+        r->right = insert(r->right, val);
     }
+    return r;
 }
 
 void inorder(struct tree *r)
@@ -45,14 +47,58 @@ void inorder(struct tree *r)
     }
 }
 
+void postorder(struct tree *r)
+{
+    if (r != NULL)
+    {
+        inorder(r->left);
+        inorder(r->right);
+        cout << r->data << " ";
+    }
+}
+
+void preorder(struct tree *r)
+{
+    if (r != NULL)
+    {
+        cout << r->data << " ";
+        inorder(r->left);
+        inorder(r->right);
+    }
+}
+
+void search(struct tree *r, int val)
+{
+    if (r != NULL)
+    {
+        if (r->data == val)
+        {
+            cout << "Element found in the Binary Tree!" << endl;
+        }
+        else if (val < r->data)
+        {
+            return search(r->left, val);
+        }
+        else
+        {
+            return search(r->right, val);
+        }
+    }
+    else
+    {
+        cout << "Element not found in the tree!" << endl;
+    }
+}
+
 int main()
 {
     int choice = 0;
+    int choice2 = 0;
     int create_counter = 0;
     int in_val = 0;
     do
     {
-        cout << "\nBST OPERATIONS\n1. Insert\n2. Display\n3. Exit" << endl;
+        cout << "\nBST OPERATIONS\n1. Insert\n2. Display\n3. Search\n4. Exit" << endl;
         cout << "Enter: " << endl;
         cin >> choice;
         switch (choice)
@@ -60,15 +106,75 @@ int main()
         case 1:
             cout << "Enter a value to insert: " << endl;
             cin >> in_val;
-            insert(root, in_val);
+            root = insert(root, in_val);
             break;
 
         case 2:
-        cout << "DISPLAY" << endl;
-        inorder(root);
-        break;
+            cout << "\nHow do you want to display?\n1. Preorder\n2. Inorder\n3. Postorder\n4. Back" << endl;
+            cin >> choice2;
+            switch (choice2)
+            {
+            case 1:
+                cout << endl;
+                for (int i = 0; i < create_counter * 3; i++)
+                {
+                    cout << "-";
+                }
+                cout << endl;
+                cout << "PREORDER DISPLAY" << endl;
+                preorder(root);
+                for (int i = 0; i < create_counter * 3; i++)
+                {
+                    cout << "-";
+                }
+                cout << endl;
 
-         case 3:
+                break;
+            case 2:
+                cout << endl;
+                for (int i = 0; i < create_counter * 3; i++)
+                {
+                    cout << "-";
+                }
+                cout << endl;
+                cout << "INORDER DISPLAY" << endl;
+                inorder(root);
+                for (int i = 0; i < create_counter * 3; i++)
+                {
+                    cout << "-";
+                }
+                cout << endl;
+
+                break;
+            case 3:
+                cout << endl;
+                for (int i = 0; i < create_counter * 3; i++)
+                {
+                    cout << "-";
+                }
+                cout << endl;
+                cout << "POSTORDER DISPLAY" << endl;
+                postorder(root);
+                for (int i = 0; i < create_counter * 3; i++)
+                {
+                    cout << "-";
+                }
+                cout << endl;
+
+                break;
+            case 4:
+                break;
+            default:
+                cout << "\nInvalid Input" << endl;
+            }
+            break;
+
+        case 3:
+            cout << "Enter a value to search: " << endl;
+            cin >> in_val;
+            search(root, in_val);
+            break;
+        case 4:
             cout << "Goodbye!" << endl;
             break;
 
@@ -76,5 +182,5 @@ int main()
             cout << "\nInvalid Input" << endl;
             break;
         }
-    } while (choice != 2);
+    } while (choice < 4);
 }
